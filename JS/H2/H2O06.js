232855a4a6ca41438bec84ae2a1381bd8bd7cc4c
Chell-Minn <151285@csgaugustinus.nl>
2020-08-20 13:51:58 +0000

    Nieuwe schooljaar

--- JS/H2/H2O06.js
+++ JS/H2/H2O06.js
@@ -25,6 +25,15 @@ function draw() {
   if (keyIsDown(RIGHT_ARROW)) {
     xJos += celGrootte;
   }
+  if (keyIsDown(LEFT_ARROW)) {
+    xJos -= celGrootte;
+  }
+  if (keyIsDown(UP_ARROW)) {
+    xJos -= celGrootte;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    xJos += celGrootte;
+  }
   
   xJos = constrain(xJos,0,width - celGrootte);
   
