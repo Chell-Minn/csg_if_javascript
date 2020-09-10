a461e74471f156b96ea5c7a504d48e96c9996354
Chell-Minn <151285@csgaugustinus.nl>
2020-09-10 13:36:06 +0000

    I LOVE RYAN!!!

--- JS/test.js
+++ JS/test.js
@@ -1,20 +1,45 @@
-var x = -50;
-
-function preload() {
-    kever = loadImage("../images/sprites/kever.png");
-}
-
 function setup() {
-    canvas = createCanvas(450,450);
+    canvas = createCanvas(450, 450);
     background('silver');
     textSize(30);
     canvas.parent('processing');
 }
 
-function draw() {
-    for (var i = 0; i < 10; i++) {
-         x+=50;
-         image(kever,x,45,50,50);
+function preload() {
+ ryan = loadImage("images/ryan.png");
+}
+
+
+var rood = {
+    x: 300,
+    y: 200,
+    dia: 10,
+
+
+    beweeg() {
+        if (keyIsDown(DOWN_ARROW)) {
+            this.y +=5;
+        }
+        if (keyIsDown(RIGHT_ARROW)) {
+            this.x +=5;
+        }
+        if (keyIsDown(UP_ARROW)) {
+            this.y -=5;
+        }
+        if (keyIsDown(LEFT_ARROW)) {
+            this.x -=5;
+        }
+    },
+    teken() {
+        fill('red');
+        noStroke();
+        background('purple')
+        image(ryan, this.x, this.y)
+        
     }
-    text("breedte = "+ kever.width,150,40)
+}
+
+function draw() { 
+    rood.teken()
+    rood.beweeg()
 }
